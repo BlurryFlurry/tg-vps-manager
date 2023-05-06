7547934029519c656693c6377d87125d56edede9
BlurryFlurry <chan@elakiri.com>
2023-05-06 20:39:37 +0530

    user create bug fix
    Signed-off-by: BlurryFlurry <chan@elakiri.com>

--- main.py
+++ main.py
@@ -58,15 +58,17 @@ async def cancel_user(update: Update, context: ContextTypes.DEFAULT_TYPE):
 async def create_user():
     logger.info(f'creating user: {user}')
     if 'expire' in user:
-        await asyncio.create_subprocess_shell(
-            f'/usr/bin/sudo /usr/sbin/useradd -M -s /usr/sbin/nologin -e $(date -d "+{user["expire"]} days" +%Y-%m-%d) "$username"')
+        shell_command = f'/usr/bin/sudo /usr/sbin/useradd -M -s /usr/sbin/nologin -e $(date -d "+{user["expire"]} days" +%Y-%m-%d) "{user["username"]}"'
+        await asyncio.create_subprocess_shell(shell_command)
+        logger.info(shell_command)
     else:
         await asyncio.create_subprocess_shell(
-            f'/usr/bin/sudo /usr/sbin/useradd -M -s /usr/sbin/nologin "$username"')
+            f'/usr/bin/sudo /usr/sbin/useradd -M -s /usr/sbin/nologin "{user["username"]}"')
 
     if 'max_logins' in user:
-        await asyncio.create_subprocess_shell(
-            f'echo "{user["username"]} hard maxlogins {user["max_logins"]}" | sudo tee -a /etc/security/limits.conf"')
+        shell_command = f'echo "{user["username"]} hard maxlogins {user["max_logins"]}" | sudo tee -a /etc/security/limits.conf'
+        await asyncio.create_subprocess_shell(shell_command)
+        logger.info(shell_command)
 
 
 async def user_create_start(update: Update, context: ContextTypes.DEFAULT_TYPE):
