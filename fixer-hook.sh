caae2bbcbe7780b4b92f792310aa4e9e2e99e742
BlurryFlurry <chan@elakiri.com>
2023-05-15 13:52:46 +0530

    hot: var order fix

--- fixer-hook.sh
+++ fixer-hook.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 ptb_service_user=$(cat "$HOME"/.config/ptb-service-user)
-LATEST_TAG=$(git ls-remote --tags "https://github.com/$GITHUB_USER/$REPO_NAME.git" | awk '{print $2}' | grep -v '{}' | sort -V | tail -n1 | sed 's|refs/tags/||')
-
 # Set Github repository details
 GITHUB_USER="BlurryFlurry"
+
 REPO_NAME="tg-vps-manager"
 LOCAL_REPO_DIR="/home/$ptb_service_user/bot"
 # Go to the local directory
 cd "$LOCAL_REPO_DIR" || exit
+LATEST_TAG=$(git ls-remote --tags "https://github.com/$GITHUB_USER/$REPO_NAME.git" | awk '{print $2}' | grep -v '{}' | sort -V | tail -n1 | sed 's|refs/tags/||')
 
 touch "$HOME"/.config/ptb-service-version.txt
 
