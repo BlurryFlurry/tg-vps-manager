553621e9500f09617efd6266b8c235ff7416824f
BlurryFlurry <chan@elakiri.com>
2023-05-15 11:33:02 +0530

    fix: open file with write mode

--- main.py
+++ main.py
@@ -179,7 +179,7 @@ async def chbanner_cancel(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 
 async def change_banner(banner):
-    with open('/etc/dropbear/banner.dat') as f:
+    with open('/etc/dropbear/banner.dat', 'w') as f:
         f.write(banner)
 
 
