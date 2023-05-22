5997ce90d16d6c12c96d844c5b1dd0a6954d4df4
BlurryFlurry <chan@elakiri.com>
2023-05-22 10:09:52 +0530

    stats message fix awk command

--- main.py
+++ main.py
@@ -338,7 +338,7 @@ async def shell_exec_stdout(command, oneline=False):
 
 async def get_service_processes():
     processes = await shell_exec_stdout(
-        """/usr/bin/ss -ntlp | /usr/bin/awk '!/Peer/ {sub("users:", "", $6);  gsub("\\\(", "", $6); gsub("\\)\\)", "", $6); split($4, a, ":"); print "Port: " a[length(a)]  " | Process: " $6 }'""")
+        """/usr/bin/ss -ntlp | /usr/bin/awk '!/Peer/ {sub("users:", "", $6);  gsub("\\\(", "", $6); gsub("\\\)\\\)", "", $6); split($4, a, ":"); print "Port: " a[length(a)]  " | Process: " $6 }'""")
     return processes
 
 async def get_server_load():
