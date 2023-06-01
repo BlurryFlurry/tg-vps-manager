53c1017ad4d603cd0711d8b8f902aec62d60b8dd
BlurryFlurry <chan@elakiri.com>
2023-06-01 10:26:02 +0530

    disable Events import temporary

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import sqlite3
 from os import environ
 from helpers import get_random_password
 from helpers import sizeof_fmt, format_bandwidth_usage
-from events import Events
+# from events import Events
 
 conn = sqlite3.connect('tgbot.db')
 c = conn.cursor()
